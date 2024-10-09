4693468a526a6d04dad1e8d268bef24509b6e59d
alexxRT <alekseev.aa@phystech.edu>
2024-10-09 12:02:08 +0300

    new: dynamic trace collection

---
+++ llvm_pass/log.c
@@ -0,0 +1,5 @@
+#include <stdio.h>
+
+void DumpTrace(char* InstructionName) {
+    printf("%s\n", InstructionName);
+}
--- llvm_pass/my_pass.cpp
+++ llvm_pass/my_pass.cpp
@@ -3,43 +3,70 @@
 #include "llvm/Passes/PassPlugin.h"
 #include "llvm/Support/raw_ostream.h"
 #include "llvm/IR/Module.h"
+#include "llvm/IR/Function.h"
 
 using namespace llvm;
 
 //pass itself
-struct MyPass : public PassInfoMixin<MyPass> {
-    // here bla bla my pass
-    PreservedAnalyses run(Module &M, ModuleAnalysisManager &AM) {
-        outs() << "Dumping statistic for module " << M.getName() << "\n";
-        for (auto &F : M) {
-            for (auto &B : F) {
-                for (auto &I : B) {
-                    // Dump Instructions with users
-                    for (auto &U : I.uses()) {
-                        Instruction *user_instr = dyn_cast<Instruction>(U.getUser());
-                        outs() << user_instr->getOpcodeName() << "<-" << I.getOpcodeName() << "\n";
+class InstructionsInfoPass : public PassInfoMixin<InstructionsInfoPass> {
+    public:
+        bool isDumpFunction (StringRef func_name) {
+              return func_name == "DumpTrace";
+        }
+
+        bool isPhiInstr (StringRef instr_name) {
+            return instr_name == "phi";
+        }
+
+        PreservedAnalyses run(Module &M, ModuleAnalysisManager &AM) {
+            outs() << "Dumping trace for module: " << M.getName() << "\n";
+
+            //Prepare builder to modify IR
+            LLVMContext &Ctx = M.getContext();
+            IRBuilder<> builder(Ctx);
+            Type *retType = Type::getVoidTy(Ctx);
+
+            // Prepare DumpTrace function
+            ArrayRef<Type *> funcStartParamTypes = {
+            builder.getInt8Ty()}; // char*
+            FunctionType *DumpTraceFuncType =
+            FunctionType::get(retType, funcStartParamTypes, false);
+
+            FunctionCallee DumpTraceFunc = M.getOrInsertFunction(
+            "DumpTrace", DumpTraceFuncType);
+            for (auto &F : M) {
+                if (isDumpFunction(F.getName()))
+                    continue;
+                for (auto &B : F) {
+                    for (auto &I : B) {
+                        // skipping loger function and phi 
+                        if (dyn_cast<Instruction>(&I) && \
+                            !isPhiInstr(I.getOpcodeName())) {
+                            builder.SetInsertPoint(&I);
+                            Value *instr_name = builder.CreateGlobalStringPtr(I.getOpcodeName());
+                            builder.CreateCall(DumpTraceFunc, {instr_name});
+                        }
                     }
                 }
             }
+            outs() << "\n";
+            return PreservedAnalyses::none();
         }
-        outs() << "\n";
-        return PreservedAnalyses::all();
-    }
+
+        static bool isRequired() { return true; }
 };
 
-// applying pass
 PassPluginLibraryInfo getPassPluginInfo() {
-  const auto callback = [](PassBuilder &PB) {
-    PB.registerPipelineStartEPCallback([&](ModulePassManager &MPM, auto) {
-      MPM.addPass(MyPass{});
-      return true;
-    });
-  };
+    const auto callback = [](PassBuilder &PB) {
+        PB.registerOptimizerLastEPCallback([&](ModulePassManager &MPM, auto) {
+            MPM.addPass(InstructionsInfoPass{});
+            return true;
+        });
+    };
 
-  return {LLVM_PLUGIN_API_VERSION, "MyPlugin", "0.0.1", callback};
-};
+    return {LLVM_PLUGIN_API_VERSION, "InstructionsInfoPlugin", "0.0.1", callback};
+}
 
-// entry point for compiler when applying my pass
 extern "C" LLVM_ATTRIBUTE_WEAK PassPluginLibraryInfo llvmGetPassPluginInfo() {
-  return getPassPluginInfo();
+    return getPassPluginInfo();
 }
